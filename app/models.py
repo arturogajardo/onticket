5ba32aacf69d2d200acc0bba414dc379c52edf5f
arturogajardo <art.gajardo@duocuc.cl>
2023-04-17 21:19:36 -0400

    Primer commit

---
+++ app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.db.models.fields import CharField, DateField, URLField
+from django.db.models.fields.files import ImageField
+from datetime import date
+
+# Create your models here.
+
+class Event(models.Model):
+    title = CharField(max_length=100)
+    description = CharField(max_length=250)
+    image = ImageField(upload_to="events/images")
+    url = URLField(blank=True)
+    date = DateField(default=date.today)
+
+    def __str__(self) -> str:
+        return self.title
---
+++ app/views.py
@@ -0,0 +1,70 @@
+from django.db import IntegrityError
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from .models import Event
+
+# Create your views here.
+
+def home(request):
+    return render(request, "home.html")
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm,
+                'error': 'Username or password is incorrect'
+            })
+        else:
+            login(request, user)
+            return redirect('home')
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+            'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('home')
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error': 'Username already taken'
+                })
+        return render(request, 'signup.html', {
+            'form': UserCreationForm,
+            'error': 'Password do not match'
+        })
+    
+@login_required 
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+def events(request):
+    events = Event.objects.all()
+    return render(request, "events.html", {"events": events})
+
+def cart(request):
+    return render(request, "cart.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
